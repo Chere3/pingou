d9de05d994f71c16fc56970e6cb1aec7a7addd84
Cheree <71246795+Chere3@users.noreply.github.com>
2026-05-10 00:29:42 -0600

    fix(bootstrap):  eliminada condicional del guild id para evitar excepciones

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,6 @@ async function boostrap() {
 	await client.start();
 	await client.uploadCommands({
 		cachePath: "./commands.json",
-		...(process.env.DEV_GUILD_ID ? { guildId: process.env.DEV_GUILD_ID } : {}),
 	});
 
 	setInterval(
